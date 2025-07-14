4f0a433cbe1ce8b429744fe0a1f7869fff12827b
zhangshaobo2020 <zhangshaobo2020@github.com>
2025-07-14 20:44:13 +0800

    project init

---
+++ src/main/java/com/zsb/blueprint/backend/BlueprintBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zsb.blueprint.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueprintBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueprintBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zsb/blueprint/backend/BlueprintBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsb.blueprint.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueprintBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
